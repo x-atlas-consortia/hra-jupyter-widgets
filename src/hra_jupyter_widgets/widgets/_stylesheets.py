c5207fe9e9e7d62ebec48c245846100915323289
Daniel Bolin <axbolin@iu.edu>
2024-08-28 10:57:13 -0400

    refactor: Big refactor

--- src/hra_jupyter_widgets/widgets/_stylesheets.py
+++ src/hra_jupyter_widgets/widgets/_stylesheets.py
@@ -1,21 +1,5 @@
-import traitlets
-
-from ..traits import Style
-
-
-class InterFontStylesheet(traitlets.HasTraits):
-    _inter_font_stylesheet = Style(
-        "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap"
-    )
-
-
-class MaterialIconsStylesheet(traitlets.HasTraits):
-    _material_icons_stylesheet = Style(
-        "https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined"
-    )
-
-
-class MaterialSymbolsStylesheet(traitlets.HasTraits):
-    _material_symbols_stylesheet = Style(
-        "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-    )
+FONTS = ["https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap"]
+MATERIAL = [
+    "https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined",
+    "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
+]
---
+++ src/hra_jupyter_widgets/widgets/_utils.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import traitlets as tr
+
+from ._constants import AttributeBindingKey, EventBindingKey
+
+
+def _snake_to_kebab_case(name: str) -> str:
+    return name.replace("_", "-")
+
+
+def get_trait_attribute_name(trait: tr.TraitType, key: str | None = None) -> str:
+    if AttributeBindingKey.AttributeName in trait.metadata:
+        return trait.metadata[AttributeBindingKey.AttributeName]
+    if key is None:
+        key = trait.name or ""
+    return _snake_to_kebab_case(key)
+
+
+def get_trait_event_name(trait: tr.TraitType, key: str | None = None) -> str:
+    if EventBindingKey.EventName in trait.metadata:
+        return trait.metadata[EventBindingKey.EventName]
+    if key is None:
+        key = trait.name or ""
+    return _snake_to_kebab_case(key).removeprefix("on-")
