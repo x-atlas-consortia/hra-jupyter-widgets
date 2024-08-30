249af1c8e1c44e83a3574c9348478c4cc97db9ac
Daniel Bolin <axbolin@iu.edu>
2024-08-30 16:29:34 -0400

    refactor: Make implementation compatible with python 3.9

--- src/hra_jupyter_widgets/widgets/_stylesheets.py
+++ src/hra_jupyter_widgets/widgets/_stylesheets.py
@@ -1,12 +1,9 @@
-import enum
-
-
-class Font(enum.StrEnum):
+class Font:
     Inter = (
         "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap"
     )
 
 
-class Material(enum.StrEnum):
+class Material:
     Icons = "https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined"
     Symbols = "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
